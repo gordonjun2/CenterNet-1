4ee648d0e41fd45ec894eac0f1d99ec17ea77cd3
DigitalHubRocks <dstadh17@gmail.com>
2020-03-13 16:49:52 +0800

    added visdrone to coco script and sh to run training

---
+++ src/train_hourglass.sh
@@ -0,0 +1,1 @@
+python main.py ctdet --gpus 1 --lr 0.156e-5 --batch_size 4 --load_model ../exp/ctdet/hourglass-52-all-dense/model_41_2.922196475507139.pth --dataset pascal_coco_tensorboard_added --arch $1
